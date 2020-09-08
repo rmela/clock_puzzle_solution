7d2001a0120979652e779516a967bc8eaf1f4baf
Robert Mela <robert.steven.mela@gmail.com>
2020-09-08 18:28:32 -0400

    Create clock.py

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# clock_puzzle_solution
-Clock coins interview question
+# Coins on Clock Puzzle
+
+Solutions to coins on clock [interview question](https://github.com/atsepkov/puzzles/blob/master/interviews/easy/coins-on-clock/README.md)
+
+Given four dimes, four nickels, and four pennies, fill a clock face with the coins by placing coins on the clock, moving forward each time
+by N hours, where N is the denomination of the coin.
+
+From the problem statement:
+
+    Here’s a quick walkthrough using the first solution, PPDDNNPDDPNN, to illustrate how the game is played. First move is a Penny
+    and it can be played anywhere so let’s put it at 6. Next move is a penny which moves us up an hour so I put that penny on 7. Next
+    is a dime which moves us up ten hours which goes past twelve o’clock and back around to 5, so I place the dime on the 5. Another
+    dime moves us to 3. And continuing with a nickel at 8, nickel at 1, penny at 2, dime at 12, dime at 10, penny at 11,
+    nickel at 4 and nickel at 9. And the sequence wins because all hours of the clock are now covered with a coin. Thanks to John Balboni
---
+++ clock.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+# Solution to interview question at https://github.com/atsepkov/puzzles/blob/master/interviews/easy/coins-on-clock/README.md
+
+from collections import namedtuple
+
+Clock = namedtuple( 'Clock', ['idx', 'coins', 'sequence' ] )
+
+def clock_to_string( clock ):
+    coins = []
+    for coin in clock.coins:
+        coins.append(
+           coin == 0 and '_' or
+           coin == 1 and 'P' or
+           coin == 5 and 'N' or
+           coin == 10 and 'D' 
+        )
+    return ''.join( coins )
+
+def clock_is_full( clock ):
+    return 64 == sum( clock.coins )
+
+def place_coin( clock, coin ):
+    newidx = ( clock.idx + coin ) % 12
+    if not clock.coins[newidx]:
+        coins = list(clock.coins )
+        sequence = list( clock.sequence )
+        sequence.append( coin )
+        coins[newidx] = coin
+        return Clock( newidx, coins, sequence )
+
+def validate( clock ):
+    idx = 0
+
+    hours = set()
+    for coin in clock.sequence:
+        idx = ( idx + coin ) % 12
+        hours.add( idx )
+
+    return len(hours) == 12
+
+def doit( clock, pennies, nickels, dimes, result ):
+
+    if clock_is_full(clock): # alternatively: pennies + nickels + dimes == 0
+        result.append( clock )
+        return
+
+    if pennies > 0:
+        newclock = place_coin( clock, 1 )
+        if newclock: doit( newclock, pennies - 1, nickels, dimes, result )
+    if nickels > 0:
+        newclock = place_coin( clock, 5 )
+        if newclock: doit( newclock, pennies, nickels - 1, dimes , result)
+    if dimes > 0:
+        newclock = place_coin( clock, 10 )
+        if newclock: doit( newclock, pennies, nickels, dimes - 1 , result)
+
+result = []
+doit( Clock( 0, [0,0,0,0,0,0,0,0,0,0,0,0], []), 4, 4, 4, result )
+
+for clock in result:
+    print( 'valid:', validate( clock ), clock_to_string( clock ) )
+
